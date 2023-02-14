de7a9f007a63505c80603e8cac94c2074ed912d8
Diba00 <dibas.shishegar@gmail.com>
2023-02-14 15:53:45 +0100

    lol

--- client1.py
+++ client1.py
@@ -1,8 +1,63 @@
-import socket
+from socket import *
+import sys
+import threading
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 5000))
+host_name = gethostname()
+serverName = gethostbyname(host_name)
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_STREAM)
 
+try:
+    clientSocket.connect((serverName, serverPort))
+except:
+    print("ConnectionError")
+    sys.exit()
+       
+username = input("Enter your username: ")
+clientSocket.send(username.encode())
+
+def receive_message():
+    while True:
+        received_message = clientSocket.recv(1024).decode()
+        if received_message:
+            print (received_message)
+
+def send_message():
+    while True:
+        sentence = input('') #kode fra Islam hvor man kan skrive en mld til server
+        clientSocket.send(sentence.encode())
+        if (sentence == "exit"):
+            clientSocket.close()
+        break
+    #modifiedSentence = clientSocket.recv(1024)
+    #print (modifiedSentence.decode())
+    
+#clientSocket.close()
+
+receive_thread = threading.Thread(target=receive_message)
+receive_thread.start()
+
+send_message()
+
+receive_thread.join()
+
+"""
 while True:
-    message = raw_input("Enter message: ")
-    client_socket.send(message)
+
+    #print("1. Send a message")
+    #print("2. Play game")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        message = input("Input message: ")
+        clientSocket.send(message.encode())
+        if (message == "exit"):
+            break
+        received_message = clientSocket.recv(1024).decode()
+        print (received_message)
+    elif choice == "2":
+        play_game()
+    else:
+        print("Invalid option. Please try again.")
+
+clientSocket.close()
+"""
--- server1.py
+++ server1.py
@@ -1,27 +1,54 @@
-import socket
+from socket import *
+import _thread as thread
+import time
 
-def broadcast(client_socket, message):
-    for sock in client_sockets:
-        if sock != client_socket:
-            sock.send(message)
+clients = []
 
-def handle_client(client_socket, address):
+def now():
+    #returns the time of day
+    
+    return time.ctime(time.time())
+
+def handleClient(connection):
+    #broadcast(f" Someone has joined the chat", connection)
+    username = connection.recv(1024).decode()
+    broadcast(f"{username} has joined the chat", connection)
     while True:
-        message = client_socket.recv(1024)
-        if not message:
+    
+        data = connection.recv(1024).decode()
+    
+        broadcast(f"\n{username}: {data}", connection)
+        if (data == "exit"):
+            clients.remove(connection)
+            broadcast(f"From Server: {username} has left the chat", connection)
+            connection.close()
             break
-        broadcast(client_socket, message)
-    client_socket.close()
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 5000))
-server_socket.listen(5)
+def broadcast(message, conn):
+    for client in clients:
+        if client != conn:
+            client.send(message.encode())
 
-client_sockets = []
 
-while True:
-    client_socket, address = server_socket.accept()
-    client_sockets.append(client_socket)
-    print("Accepted connection from", address)
-    client_thread = threading.Thread(target=handle_client, args=(client_socket, address))
-    client_thread.start()
+def main():
+    #creates a server socket, listens for new connections,
+    #and spawns a new thread whenever a new connection join
+    
+    serverPort = 12000
+    serverSocket = socket(AF_INET,SOCK_STREAM)
+    try:
+        serverSocket.bind(('',serverPort))
+    except: 
+        print("Bind failed. Error : ")
+    serverSocket.listen(1)
+    print ('The server is ready to receive')
+    while True:
+        connectionSocket, addr = serverSocket.accept() 
+        clients.append(connectionSocket)
+        print('Server connected by ', addr) 
+        print('at ', now())
+        thread.start_new_thread(handleClient, (connectionSocket,)) 
+    serverSocket.close()
+
+if __name__ == '__main__':
+    main()
